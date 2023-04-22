fedd428770ce97789b731235fa3e455eb8486e52
raufukiwala <raufukiwala@gmail.com>
2023-04-22 16:21:43 +0200

    Added exercise 4 solution

--- coursera_sql_errors.sql
+++ coursera_sql_errors.sql
@@ -46,6 +46,28 @@ JOIN dsv1069.orders AS o
 ON  i.id = o.item_id;
 
 
+/*
+Exercise 4:
+--Goal: Check out the query below. This is not the right way to count the number of viewed_item
+events. Determine what is wrong and correct the error.
+
+Starter Code:
+SELECT
+COUNT(event_id) AS events
+FROM dsv1069.events
+WHERE event_name = ‘view_item’
+*/
+
+-- SOLUTION
+
+-- Table name is missing in the WHERE clause
+
+SELECT 
+COUNT(event_id) AS events
+FROM dsv1069.events
+WHERE events.event_name = 'view_item';
+
+
 
 
 
