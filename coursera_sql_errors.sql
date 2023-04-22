3d3f57316cfe0562920cae64c400b3d2aa611937
raufukiwala <raufukiwala@gmail.com>
2023-04-22 15:57:55 +0200

    added queries for exercise 2 and 3

--- coursera_sql_errors.sql
+++ coursera_sql_errors.sql
@@ -14,7 +14,38 @@ SELECT email_address
 FROM dsv1069.users
 WHERE deleted_at IS NULL;
 
--- OUTPUT
+
+/*
+Exercise 2:
+--Goal: Use the items table to count the number of items for sale in each category
+
+Starter Code: (none)
+*/
+
+-- SOLUTION
+
+SELECT  category,
+        COUNT(category) AS total_cat_items
+FROM dsv1069.items
+GROUP BY category;
+
+
+/*
+Exercise 3:
+--Goal: Select all of the columns from the result when you JOIN the users table to the orders
+table
+
+Starter Code: (none)
+*/
+
+-- SOLUTION
+
+SELECT *
+FROM dsv1069.items AS i
+JOIN dsv1069.orders AS o
+ON  i.id = o.item_id;
+
+
 
 
 
